50a3eba2b03dd42b9b383050cd61abe0ac149ab2
Valentin Todorov <valentin.todorov@chello.at>
2016-11-11 12:09:33 +0000

    version 0.2-5

--- R/AllClasses.R
+++ R/AllClasses.R
@@ -50,3 +50,46 @@ setClass("PredictSimca", representation(classification = "factor",
                                       sdsc = "matrix",
                                       ct="Utable"))
 setClass("SummarySimca", representation(simcaobj = "Simca"))
+
+###################### SosDisc ####################################
+setClass("SosDisc", representation(
+                               call = "language",
+                               prior = "vector",
+                               counts = "vector",
+
+                               beta = "matrix",               # Q coefficient vectors of the predictor matrix from optimal scoring
+                               theta = "matrix",              # Q coefficient vectors of the dummy matrix for class coding from optimal scoring
+                               lambda = "numeric",            # L1 norm penaly parameter
+                               varnames = "character",        # vector of names of selected predictor variables
+##                               varIndex = "integer",          # We will remove this (use varnames instead)
+##                               origP = "numeric",             # We will remove this (get it as ncol(X))
+##                               rss = "numeric",               # We will remove this: vector of length Q with weighted residual sum of squares plus L1 penaltyterm (weighted lasso cost) from optimal scoring
+
+                               ## centering and scaling: later we can add parameters center and scale which can be (TRUE or FALSE, any function or vectors with lengthp)
+                               ##   see Pca
+                               center = "vector",             # centering vector of the predictors (coordinate wise median)
+                               scale = "vector",              # scaling vector of the predictors (mad)
+
+                               fit = "Linda",                 # Linda model (robust LDA model) from the low dimensional subspace
+                               mahadist2="vector",            # fixme: These will go later to Linda object: squared robust Mahalanobis distance (calculated with estimates from Linda) to the group center in the low dimensional subspace
+                               wlinda = "vector",             # fixme: These will go later to Linda object: weights derived from mahadist2
+
+##                               wlts = "matrix",             # We remove these (not needed): Q weighting vectors from initial sparseLTS estimate
+##                               wrew = "matrix",             # We remove these (not needed):Q weighting vectors from reweighting step
+
+                               X = "Umatrix",                 # can be missing
+                               grp = "factor",
+                               "VIRTUAL"))
+
+setClass("SummarySosDisc", representation(obj = "SosDisc"))
+setClass("SosDiscClassic", contains="SosDisc")
+setClass("SosDiscRobust", contains="SosDisc")
+
+##setClass("PredictSosDisc", representation(classification = "factor",
+##                                      posterior = "matrix",
+##                                      x = "matrix",
+##                                      ct="Utable"))
+##
+setClass("PredictSosDisc", representation(classification = "factor",
+                                        w = "vector",
+                                        mahadist2="matrix"))
